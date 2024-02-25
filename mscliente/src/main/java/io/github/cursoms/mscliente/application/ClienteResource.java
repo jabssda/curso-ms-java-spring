f20c61c6cfe6a0c97ae26bc80f236cfda545b53c
jabssda <jabssda@gmail.com>
2024-02-25 11:14:50 -0300

    ms cliente

--- mscliente/src/main/java/io/github/cursoms/mscliente/application/ClienteResource.java
+++ mscliente/src/main/java/io/github/cursoms/mscliente/application/ClienteResource.java
@@ -1,14 +1,47 @@
 package io.github.cursoms.mscliente.application;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.github.cursoms.mscliente.application.representation.ClienteSaveRequest;
+import io.github.cursoms.mscliente.application.service.ClienteService;
+import io.github.cursoms.mscliente.domain.Cliente;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("clientes")
+@RequiredArgsConstructor
 public class ClienteResource {
+
+    private  final ClienteService clienteService;
     @GetMapping
     public String status(){
         return "ok";
+    }
+
+
+    @PostMapping
+    public ResponseEntity save(@RequestBody ClienteSaveRequest request){
+        Cliente cliente = request.toModel();
+        clienteService.save(cliente);
+        URI headerLocation = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .query("cpf={cpf}")
+                .buildAndExpand(cliente.getCpf())
+                .toUri();
+        return ResponseEntity.created(headerLocation).build();
+    }
+
+
+    @GetMapping(params = "cpf")
+    public ResponseEntity dadosCliente( @RequestParam("cpf") String cpf){
+        var cliente = clienteService.getByCPF(cpf);
+        if (cliente.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(cliente);
     }
 }
--- mscliente/src/main/java/io/github/cursoms/mscliente/domain/Cliente.java
+++ mscliente/src/main/java/io/github/cursoms/mscliente/domain/Cliente.java
@@ -19,7 +19,12 @@ public class Cliente {
     private Long id;
     private String cpf;
     private String nome;
+    private Integer idade;
 
 
-
+    public Cliente(String cpf, String nome, Integer idade) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.idade = idade;
+    }
 }
